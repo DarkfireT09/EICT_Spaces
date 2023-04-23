3840200986ab4081a9b59e5d1029afcbae33ee42
Feligx <feligxgamer@gmail.com>
2023-04-23 17:47:02 -0500

    Fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eict_scheduling_test1/pages/SpaceDetail.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:eict_scheduling_test1/utils/utils.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
@@ -37,6 +40,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  var db = FirebaseFirestore.instance;
+
+  /*getSpaces(db);*/
 
   @override
   Widget build(BuildContext context) {
---
+++ lib/utils/utils.dart
@@ -0,0 +1,7 @@
+void getSpaces(db) async {
+  await db.collection("users").get().then((event) {
+    for (var doc in event.docs) {
+      print("${doc.id} => ${doc.data()}");
+    }
+  });
+}
