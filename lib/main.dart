ad80a655c219252259adc08730439d1678c26fc9
dleyvacastro <dleyvacastro@gmail.com>
2023-04-23 17:16:47 -0500

    Firebase configs fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:eict_scheduling_test1/pages/SpaceDetail.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
