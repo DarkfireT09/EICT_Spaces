f290ea8c1c166173e11220089e029c81dd0e45c7
Feligx <feligxgamer@gmail.com>
2023-04-23 20:04:11 -0500

    Added home builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:eict_scheduling_test1/pages/SpaceDetail.dart';
+import 'package:eict_scheduling_test1/utils/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-import 'package:eict_scheduling_test1/utils/utils.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,17 +16,13 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-          useMaterial3: true
-      ),
-      home: const MyHomePage(title: 'EICT Spaces')
-    );
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(useMaterial3: true),
+        home: const MyHomePage(title: 'EICT Spaces'));
   }
 }
 
@@ -40,97 +36,155 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
-  var db = FirebaseFirestore.instance;
-
-  
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.logout),
-              onPressed: () {},
-            ),
-          ],
-          title: Text(widget.title),
-        ),
-        body: RefreshIndicator(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.logout),
+            onPressed: () {},
+          ),
+        ],
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
           onRefresh: () async {
             await Future.delayed(const Duration(seconds: 1));
             setState(() {});
           },
-          child: Center(
-              child: GridView.count(
-                crossAxisCount: 2,
-                childAspectRatio: 2 / 3,
-                children: List.generate(10, (index) {
-                  return Card(
-                    elevation: 0,
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Theme.of(context).colorScheme.outline,
-                      ),
-                      borderRadius: const BorderRadius.all(Radius.circular(12)),
-                    ),
-                    child: InkWell(
-                        customBorder: RoundedRectangleBorder(
-                          side: BorderSide(
-                            color: Theme.of(context).colorScheme.outline,
-                          ),
-                          borderRadius: const BorderRadius.all(Radius.circular(12)),
+          child: FutureBuilder(
+            future: getSpaces(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasData) {
+                  List? spaces = snapshot.data?.toList();
+                  return Center(
+                    child: GridView.builder(
+                        itemCount: spaces?.length,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 2,
+                          childAspectRatio: 2 / 3,
                         ),
-                        onTap: () {
-                          print('Room $index');
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const SpaceDetail(),
-                            ),
-                          );
-                        },
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            ClipRRect(
-                              //width: double.infinity,
+                        itemBuilder: (context, index) {
+                          return Card(
+                            elevation: 0,
+                            shape: RoundedRectangleBorder(
+                              side: BorderSide(
+                                color: Theme.of(context).colorScheme.outline,
+                              ),
                               borderRadius:
-                              const BorderRadius.all(Radius.circular(12)),
-                              child: Image.network(
-                                'https://picsum.photos/500',
-                                width: double.infinity,
-                              ),
+                                  const BorderRadius.all(Radius.circular(12)),
                             ),
-                            Container(
-                              margin: const EdgeInsets.only(top: 24),
-                              child: Column(
-                                children: [
-                                  Text(
-                                    'Room $index',
-                                    style: Theme.of(context).textTheme.headline6,
+                            child: InkWell(
+                                customBorder: RoundedRectangleBorder(
+                                  side: BorderSide(
+                                    color:
+                                        Theme.of(context).colorScheme.outline,
                                   ),
-                                  Text(
-                                    'Disponible hoy ${true}',
-                                    style: Theme.of(context).textTheme.bodyText2,
-                                  ),
-                                ],
-                              ),
-                            )
-                          ],
-                        )),
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(12)),
+                                ),
+                                onTap: () {
+                                  print('Room $index');
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => const SpaceDetail(),
+                                    ),
+                                  );
+                                },
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: [
+                                    ClipRRect(
+                                      //width: double.infinity,
+                                      borderRadius: const BorderRadius.all(
+                                          Radius.circular(12)),
+                                      child: Image.network(
+                                        'https://picsum.photos/500',
+                                        width: double.infinity,
+                                      ),
+                                    ),
+                                    Container(
+                                      margin: const EdgeInsets.only(top: 16),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            spaces?[index]['name'] ??
+                                                'Sin nombre',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .titleLarge,
+                                          ),
+                                          Text(
+                                            '${spaces?[index]['campus']} - ${spaces?[index]['location']}' ??
+                                                'Sin ubicación',
+                                            style: Theme.of(context)
+                                                .textTheme
+                                                .bodyMedium,
+                                          ),
+                                          Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.center,
+                                              children: [
+                                                const Icon(Icons.people),
+                                                Text(
+                                                  '${spaces?[index]['student_capacity'] ?? 'Sin capacidad'}',
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodyMedium,
+                                                ),
+                                                const SizedBox(
+                                                  width: 24,),
+                                                const Icon(Icons.computer),
+                                                Text(
+                                                  '${spaces?[index]['equipment_amount'] ?? 'Sin equipos'}',
+                                                  style: Theme.of(context)
+                                                      .textTheme
+                                                      .bodyMedium,
+                                                ),
+                                              ]),
+
+                                        ],
+                                      ),
+                                    )
+                                  ],
+                                )),
+                          );
+                        }),
                   );
-                }),
-              ),
-            )
-        ),
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text(
+                      'Error: ${snapshot.error}',
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                  );
+                } else {
+                  return const Center(
+                    child: Text(
+                      'No data',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  );
+                }
+              }
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ),
-        );
+    );
   }
 }
--- lib/utils/utils.dart
+++ lib/utils/utils.dart
@@ -1,7 +1,30 @@
-void getSpaces(db) async {
+/*void getSpaces(db) async {
   await db.collection("users").get().then((event) {
     for (var doc in event.docs) {
       print("${doc.id} => ${doc.data()}");
     }
   });
+}*/
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+Future<List> getSpaces() async {
+  var db = FirebaseFirestore.instance;
+  List spacesList = [];
+  var spacesCollection = db.collection('spaces');
+  QuerySnapshot querySnapshot = await spacesCollection.get();
+  List<QueryDocumentSnapshot> spaces = querySnapshot.docs;
+  return spaces.map((e) => ({
+    'id': e.id,
+    'name': e['name'],
+    'amount': e['amount'],
+    'area': e['area'],
+    'campus': e['campus'],
+    'categories': e['categories'],
+    'dependency': e['dependency'],
+    'equipment_amount': e['equipment_amount'],
+    'location': e['location'],
+    'services': e['services'],
+    'student_capacity': e['student_capacity'],
+  })).toList();
 }
