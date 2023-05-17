88266620b75b4edca8ad6090d8d202acc9e12309
DarkfireT09 <camilof002@gmail.com>
2023-05-16 20:42:50 -0500

    modulo de diligenciar datos agregado

--- lib/pages/date_picker.dart
+++ lib/pages/date_picker.dart
@@ -5,6 +5,8 @@ import 'day_Selection.dart';
 class DatePicker extends StatefulWidget {
   DatePicker({Key? key}) : super(key: key);
 
+
+
   @override
   State<DatePicker> createState() => _DatePickerState();
 }
@@ -12,47 +14,78 @@ class DatePicker extends StatefulWidget {
 class _DatePickerState extends State<DatePicker> {
   DateTime _selectedDate = DateTime.now();
 
+  TextEditingController controller_name = TextEditingController();
+  TextEditingController controller_email = TextEditingController();
+  TextEditingController controller_phone = TextEditingController();
+  TextEditingController controller_description = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Detallas del espacio'),
+        title: const Text('Rellena tus datos'),
       ),
       body: Center(
         //put a detepicker here
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Container(
-              margin: const EdgeInsets.only(bottom: 20),
-              child: Text(
-                'Dia de la reserva:',
-                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.black),
+            Center(
+              child: Container(
+                padding: const EdgeInsets.only(left: 20, right: 20),
+                margin: const EdgeInsets.only(bottom: 20),
+                child: const Text(
+                  'Porfavor diligencie los siguientes datos:',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )
+                )
               ),
             ),
-            Text(
-              //DateTime.now().day.toString() + '/' + DateTime.now().month.toString() + '/' + DateTime.now().year.toString(),
-              _selectedDate.day.toString() + '/' + _selectedDate.month.toString() + '/' + _selectedDate.year.toString(),
-              style: TextStyle(fontSize: 20),
+            Container(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: TextField(
+                controller: controller_name,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Nombre completo',
+                ),
+              ),
             ),
             Container(
-              margin: const EdgeInsets.only(top: 20),
-              child: ElevatedButton(
-                  onPressed: ()async{
-                    final dia = await showDatePicker(
-                        context: context,
-                        initialDate: DateTime.now(),
-                        firstDate: DateTime.now(),
-                        lastDate: DateTime.now().add(const Duration(days: 30))
-                    );
-
-                    if(dia != null){
-                      setState(() {
-                        _selectedDate = dia;
-                      });
-                    }
-                  },
-                  child: const Text('Seleccionar fecha')
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: TextField(
+                controller: controller_email,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Correo electrónico',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: TextField(
+                controller: controller_phone,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Número de teléfono',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(left: 20, right: 20),
+              margin: const EdgeInsets.only(bottom: 20),
+              child: TextField(
+                controller: controller_description,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Descripción del uso que le piensa dar al espacio',
+                ),
               ),
             ),
             Container(
@@ -65,7 +98,7 @@ class _DatePickerState extends State<DatePicker> {
                     );
 
                   },
-                  child: const Text('Reservar')
+                  child: const Text('Continuar')
               ),
             ),
           ],
